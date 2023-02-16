a1eb6ce54ec6acabcf519809cb1073908a62ae39
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-02-16 21:11:50 +0200

    ./

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,14 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openMenuBtn: document.querySelector("[data-modal-open]"),
+      closeMenuBtn: document.querySelector("[data-modal-close]"),
+      menu: document.querySelector("[data-modal]"),
     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openMenuBtn.addEventListener("click", toggleModal);
+    refs.closeMenuBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
     }
   })();
